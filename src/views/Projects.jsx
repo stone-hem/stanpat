6abc7154bf11de173c527a3e4397ea1a4520741b
Hemstone <hemstone456@gmail.com>
2023-08-11 16:18:39 +0300

    more add

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -18,20 +18,20 @@ const Projects = () => {
             </p>
           </Bottom>
         </Project>
-        <Project>
+        <ProjectOne>
           <Bottom>
             <p>
               Britam Towers for British American Insurance Ltd (Kenya) at Upper
               hill Nairobi.
             </p>
           </Bottom>
-        </Project>
-        <Project>
+        </ProjectOne>
+        <ProjectTwo>
           <Bottom>
             <p>Tenwek Mission Hospital Eye and Dental Clini</p>
           </Bottom>
-        </Project>
-        <Project>
+        </ProjectTwo>
+        <ProjectThree>
           <Bottom>
             <p>
               Lusail Expressway Artscape in Doha City–Design Engineer
@@ -41,65 +41,24 @@ const Projects = () => {
               liasion.
             </p>
           </Bottom>
-        </Project>
-        <Project>
+        </ProjectThree>
+        <ProjectFour>
           <Bottom>
             <p>
               Al Farjan Projects, four sports facilities (stadia) for the Qatar
               Olympic Committee: AECOM Project.
             </p>
           </Bottom>
-        </Project>
-        <Project>
+        </ProjectFour>
+        <ProjectFive>
           <Bottom>
             <p>
               American Community School- Admin block, Tuition Blocks, Libraries
               and Sports facilities
             </p>
           </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            St Theresa&apos;s Girls Hostel - Eastligh Nairobi – Complete
-            </p>
-          </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            60 Ruaka Apartments - for Safaricom Investment Company- Complete
-            </p>
-          </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            KCB bank -Advantage Gold branches -UN Gigiri – Complete
-            </p>
-          </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            Nursery Expansion -Loreto Msongari – Complete
-            </p>
-          </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            Naivas Hotel - Naivas Supermarket-Done
-            </p>
-          </Bottom>
-        </Project>
-        <Project>
-          <Bottom>
-            <p>
-            25 Parklane Executive Apartments-Parklands – ongoing
-            </p>
-          </Bottom>
-        </Project>
+        </ProjectFive>
+       
       </Featured>
     </Section>
   );
@@ -119,7 +78,57 @@ const Bottom = styled.div`
 const Project = styled.div`
   height: 200px;
   margin: 5px;
+  background-image: url("rivers.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const ProjectOne = styled.div`
+  height: 200px;
+  margin: 5px;
+  background-image: url("britam.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const ProjectTwo = styled.div`
+  height: 200px;
+  margin: 5px;
+  background-image: url("tenwek.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const ProjectThree = styled.div`
+  height: 200px;
+  margin: 5px;
+  background-image: url("lusail.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const ProjectFour = styled.div`
+  height: 200px;
+  margin: 5px;
   background-image: url("construction.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  align-items: flex-end;
+`;
+
+const ProjectFive = styled.div`
+  height: 200px;
+  margin: 5px;
+  background-image: url("american.jpeg");
   background-repeat: no-repeat;
   background-size: cover;
   display: flex;
